e970e7954c6ce72bcf56c819b9412ac5d5bd4ec5
Anwarsk9 <anwarsk91678@gmail.com>
2024-01-10 14:59:58 +0530

    implemented account route with selected name by user.

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,10 +1,14 @@
 import { getServerSession } from "next-auth";
 import React from "react";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 const Account = async () => {
-  //@ts-ignore
-  const { user } = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return redirect("/");
+  }
+  const { user } = session;
   return <div>Account {user ? user?.name : " "}</div>;
 };
 
