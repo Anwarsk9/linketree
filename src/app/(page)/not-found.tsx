8d3153749a3277e009b79f80931a94da6ec15290
Anwarsk9 <anwarsk91678@gmail.com>
2024-01-24 09:49:42 +0530

    created not-found.tsx for user not-found message.

---
+++ src/app/(page)/not-found.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="bg-black text-white h-screen">
+      <div className="flex justify-center items-center h-screen  gap-5 text-2xl">
+        <span>😶 404</span>
+        <span> | </span>
+        <span>User Not  Found</span>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
