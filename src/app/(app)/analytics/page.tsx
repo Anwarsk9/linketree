7cdf255bcd450be60a38cfafdfe64cbaf24792d7
Anwarsk9 <anwarsk91678@gmail.com>
2024-03-01 18:07:23 +0530

    implemented chart from react-rechats.

--- src/app/(app)/analytics/page.tsx
+++ src/app/(app)/analytics/page.tsx
@@ -1,16 +1,63 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Chart from "@/components/chart/Chart";
 import { Event } from "@/models/Event";
-import React from "react";
+import { Page } from "@/models/Page";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 const Analytics = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/");
+  }
+  const page = await Page.findOne({ owner: session.user?.email });
   const viewsCount = await Event.countDocuments({
     type: "view",
-    uri: "anwars",
+    uri: page.uri,
   });
-  const clickCount = await Event.countDocuments({ type: "click" });
+  const clickCount = await Event.countDocuments({
+    type: "click",
+    uri: page.links.map((link) => link.url),
+  });
+
+  const userAnalytics = await Event.aggregate(
+    [
+      {
+        $match: {
+          type: "view",
+          uri: page.uri,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              date: "$createdAt",
+              format: "%Y-%m-%d",
+            },
+          },
+          count: {
+            $count: {},
+          },
+        },
+      },
+    ],
+    { $order: "-_id" }
+  );
+
   return (
     <div className="w-full h-full p-6 bg-white">
-      <div className="">{viewsCount}</div>
-      <div className="">{clickCount}</div>
+      <div className="">views: {viewsCount}</div>
+      <div className="">clicks: {clickCount}</div>
+      <div>
+        {userAnalytics.map(({ _id: date, count }) => (
+          <div>
+            {date}: {count}
+          </div>
+        ))}
+      </div>
+      <Chart data={userAnalytics} />
     </div>
   );
 };
