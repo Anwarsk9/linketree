d982e3d32121445b93972cf0db615d8292368248
Anwarsk9 <anwarsk91678@gmail.com>
2024-03-02 19:45:54 +0530

    Created & updated paths for authOption imports.

--- src/actions/AccountFormData.tsx
+++ src/actions/AccountFormData.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/libs/auth";
 import { Page } from "@/models/Page";
 import { getServerSession } from "next-auth";
 
